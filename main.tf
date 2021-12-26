aefc0a299b8799736f507975b10bba7d1b808638
suresh <cloudeng65@gmail.com>
2021-12-26 14:42:01 +0530

    vpc removed

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "google" {
   region  = "us-central1"  
   zone        = "us-central1-b"
 }
-resource "google_compute_network" "vpc_networ" {
-  name                    = "vpc-networ"
-  auto_create_subnetworks = true
-  mtu                     = 1460
-}
+#resource "google_compute_network" "vpc_networ" {
+#  name                    = "vpc-networ"
+#  auto_create_subnetworks = true
+#  mtu                     = 1460
+#}
