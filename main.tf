e3a9f9594016254aabf2ecd6809cd92ecdc4b5bd
sk-kishtwar <96495361+sk-kishtwar@users.noreply.github.com>
2021-12-26 14:06:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "google" {
   region  = "us-central1"  
   zone        = "us-central1-b"
 }
-resource "google_compute_network" "vpc_networke" {
-  name                    = "vpc-networke"
+resource "google_compute_network" "vpc_networ" {
+  name                    = "vpc-networ"
   auto_create_subnetworks = true
   mtu                     = 1460
 }
